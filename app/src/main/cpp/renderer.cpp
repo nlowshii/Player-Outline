8bf131533ab719f5431998388c1aa701654122ef
nlowshii <luxxstl14@gmail.com>
2026-05-26 16:29:28 +0700

    Create renderer.cpp

---
+++ app/src/main/cpp/renderer.cpp
@@ -0,0 +1,12 @@
+#include "renderer.h"
+#include "outline.h"
+#include "utils.h"
+
+void RenderOutline() {
+
+    EnableOutline();
+
+    LOGI("Rendering Local Player Outline");
+
+    DisableOutline();
+}
